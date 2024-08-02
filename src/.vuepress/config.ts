a474f8bbc9a5a06511a5eae82fb395c259b2b40a
是青旨啊🌴 <i@sayqz.com>
2024-08-02 22:14:02 +0800

    更新路径

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/tunefree_docs/",
+  base: "/",
 
   lang: "zh-CN",
   title: "TuneFree",
