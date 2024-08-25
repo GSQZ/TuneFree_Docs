0cd4b3d1b5036881b35246df9220681ed336dbcb
是青旨啊🌴 <i@sayqz.com>
2024-08-25 18:11:53 +0800

    docs: 更新白名单文档和频道守则的链接

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -7,7 +7,19 @@ export default sidebar({
     {
       text: "使用文档",
       icon: "laptop-code",
-      prefix: "docs/s",
+      prefix: "docs/使用文档",
+      children: "structure",
+    },
+    {
+      text: "白名单文档",
+      icon: "laptop-code",
+      prefix: "docs/白名单文档",
+      children: "structure",
+    },
+    {
+      text: "频道守则",
+      icon: "scale-balanced",
+      prefix: "docs/频道守则",
       children: "structure",
     },
   ],
